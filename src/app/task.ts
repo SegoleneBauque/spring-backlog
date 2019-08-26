ac963715b4b7305fb18048e09f30f4b705795844
Segolene Bauque <sego.bauque@gmail.com>
2019-08-26 16:07:29 +0200

    modification component task creation classe task.ts creation d'un service task

---
+++ src/app/task.ts
@@ -0,0 +1,8 @@
+export class Task {
+  id: number;
+  titre: string;
+  item: string;
+  personInCharge: string;
+  etat: string;
+  definitionOfDone: string;
+}
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from '../task';
 
 @Component({
   selector: 'app-task',
@@ -7,12 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit {
 
-  id: number = 1;
-  titre: string = 'première tâche';
-  item: string = "première étape de l'appli";
-  personInCharge: string = "David Schieffer";
-  etat: string = 'Todo';
-  definitionOfDone: string = 'Moi en tant que développeur, je veux voir apparaître ma première tâche sur mon application Angular';
+  task: Task;
 
   constructor() { }
 
