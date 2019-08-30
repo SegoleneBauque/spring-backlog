9676292210fffeb27fa695165696949367030b71
Segolene Bauque <sego.bauque@gmail.com>
2019-08-30 12:20:01 +0200

    implémentation du newItemComponent et mise en place du CSS correspondant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import {HomeComponent} from "./home/home.component";
 import {TaskComponent} from "./task/task.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {ItemComponent} from "./item/item.component";
+import {NewItemComponent} from "./new-item/new-item.component";
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'items', component: NewItemComponent},
   {path: 'items/:id', component: ItemComponent},
   {path: 'tasks/:id', component: TaskComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
